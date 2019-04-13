0f92009122f692ee35779bfecd196341369b5b99
Gildas GH <gildas.gh@protonmail.ch>
2019-04-13 17:02:07 +0200

    readme, config.json example file

---
+++ README.md
@@ -0,0 +1,15 @@
+<div align="center">
+  ![bop](https://cdn.becauseofprog.fr/logos/bop-transparent.png)
+  # BecauseOfProg's IT blog
+  ## blog.becauseofprog.fr
+  ![version](https://img.shields.io/badge/version-3.0--beta-yellow.svg)
+</div>
+
+## Installation
+
+### Requirements
++ Python 3.5+
++ Install requirements with pip : ``pip install -r requirements.txt``
+
+### Project setup
+Coming soon. Clone the repository and then add a systemd service to serve the blog with uWSGI (a configuration file for uWSGI is included: blog-bop-v3.ini)
--- app.py
+++ app.py
@@ -2,23 +2,11 @@ from flask import Flask
 from utils.image_proxy import image_proxy
 from utils.links import get_hostname, clean_descr
 from utils.time_to_rss import time_to_rss
-
 import datetime
+
 
 application = Flask(__name__)
 application.secret_key = "HYP_7qbKg(*+A+h:"
-
-# application.config.update(
-#     TESTING=True,
-#     SECRET_KEY=b'HYP_7qbKg(*+A+h:'
-
-# )
-
-# application.jinja_env.globals.update(image_proxy=image_proxy)
-# application.jinja_env.globals.update(get_hostname=get_hostname)
-# application.jinja_env.globals.update(clean_descr=clean_descr)
-# application.jinja_env.globals.update(datetime=datetime)
-# application.jinja_env.globals.update(time_to_rss=time_to_rss)
 
 application.add_template_global(name='image_proxy', f=image_proxy)
 application.add_template_global(name='get_hostname', f=get_hostname)
