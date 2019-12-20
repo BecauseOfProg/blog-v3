15d1b3f26e3a7ef7d27c304f680b27ab0d884447
Gildas GH <gildas.gh@protonmail.ch>
2019-12-20 22:01:10 +0100

    Refactor : integrated the new image-proxy service - changed imag_proxy() function to allow dynamic height and width - Now using this proxy on pictures of the homepage
    TODO : integrate it on the other pages
    
    (Updated misc.styl, with a few changes)

--- core/utils/image_proxy.py
+++ core/utils/image_proxy.py
@@ -1,17 +1,24 @@
-from urllib.parse import urlparse
+from urllib.parse import urlparse, quote
 
 '''This file contains the function image_proxy'''
 
 
 def image_proxy(url, width, height):
     '''Optimize images and pictures by resizing them to apppropriate sizes
-    and reducing their size. Currently we're using a reverse proxy
-    and the real pictures comes from lesnumeriques.com'''
-    proxy = "https://image-proxy.becauseofprog.fr/"
+    and reducing their size. We're using Photon, a PHP application used on
+    WordpPress to proxify and dynamically resize pictures. '''
+    
+    proxy = "https://i.cdn.becauseofprog.fr/"
     o = urlparse(url)
-    if width == 0 or height == 0:
-        return proxy+o.netloc+o.path
-        # Don't try to resize pictures if the width or heigth equals to 0
+    
+    if width == 0 and height == 0:
+        ret =  proxy+o.netloc+o.path
+        # Don't try to resize pictures if the width and heigth equals to 0
+    elif width == 0:
+        ret = proxy+o.netloc+o.path+"?h="+str(height)
+    elif height == 0:
+        ret = proxy+o.netloc+o.path+"?w="+str(width)
     else:
-        out = proxy+o.netloc+o.path+"?resize="+str(width)+","+str(height)
-        return out
+        ret = proxy+o.netloc+o.path+"?resize="+str(width)+","+str(height)
+    
+    return ret
