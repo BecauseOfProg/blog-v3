d0a35fce20d6b600e8729b2fe8257412c1841b9d
Gildas GH <gildas.gh@protonmail.ch>
2020-03-18 22:00:39 +0100

    Fix for Materialize Parallaxes

--- resources/static/js/materialize-init.js
+++ resources/static/js/materialize-init.js
@@ -35,4 +35,7 @@ document.addEventListener("DOMContentLoaded",function(){
 
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems);
+
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
 });
