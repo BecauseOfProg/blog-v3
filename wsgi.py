060fe30aa02a88f9f359cd1475b10ac3b222dbcf
Théo Vidal <exybore@becauseofprog.fr>
2020-07-26 20:33:11 +0200

    fix: fix database mapping

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 from core.database import db
+from app.models import article, user, post
+
+db.generate_mapping(create_tables=False)
+
 from core.app import application
 
 from config import routes
 from config import bundler
 
 if __name__ == '__main__':
-    db.generate_mapping(create_tables=False)
     application.run()
